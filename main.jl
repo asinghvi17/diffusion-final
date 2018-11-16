afa01f098c57b8b805c1bef1b8f1db4e4916a2ef
asinghvi17 <anshulsinghvi@gmail.com>
2018-11-16 14:23:53 -0500

    commented on equation

--- main.jl
+++ main.jl
@@ -1,8 +1,21 @@
 #!/usr/bin/env julia
 
-using Plots, PlotThemes, ProgressBar, StatsBase
+using   Plots,        # for plotting
+        PlotThemes,   # to theme plots - :dark, :wong, :lime
+        ProgressBar,  # just for lulz
+        StatsBase     # histogram
 
-gr();
+gr();   # set the plot backend; for publication figures, use pgfplots.  GR is a nice quick framework that just works.
+
+
+# ordinary differential equation of diffusion:
+# ∂Ψ/∂t = D⋅∂²Ψ/∂x²
+# ⟹ Ψ = cos(ax)⋅exp(bt) such that a = Db
+
+# change to stochastic
+# ∂Ψ/∂t = D⋅∂²Ψ/∂x² + ⅊
+# where ⅊ is noise
+# can only be solved analytically
 
 a = 1;
 b = 1;
