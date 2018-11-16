700c167187322ab2f0e3949f7ec847c171faecd2
asinghvi17 <anshulsinghvi@gmail.com>
2018-11-16 14:41:25 -0500

    wrapped code in function for better optimization

--- main.jl
+++ main.jl
@@ -1,23 +1,26 @@
 #!/usr/bin/env julia
 # add flags: -O3 --compile=all
 
-using   Plots,        # for plotting
-        PlotThemes,   # to theme plots - :dark, :wong, :lime
-        ProgressMeter,  # just for lulz
-        StatsBase     # histogram
 
-gr();   # set the plot backend; for publication figures, use pgfplots.  GR is a nice quick framework that just works.
+function main()
+    using   Plots,        # for plotting
+            PlotThemes,   # to theme plots - :dark, :wong, :lime
+            ProgressMeter,  # just for lulz
+            StatsBase     # histogram
+
+    gr();   # set the plot backend; for publication figures, use pgfplots.  GR is a nice quick framework that just works.
 
 
-# ordinary differential equation of diffusion:
-# ∂Ψ/∂t = D⋅∇²Ψ
-# in one dimension:
-# ∂Ψ/∂t = D⋅∂²Ψ/∂x²
-# ⟹ Ψ = cos(ax)⋅exp(bt) such that a = Db
+    # ordinary differential equation of diffusion:
+    # ∂Ψ/∂t = D⋅∇²Ψ
+    # in one dimension:
+    # ∂Ψ/∂t = D⋅∂²Ψ/∂x²
+    # ⟹ Ψ = cos(ax)⋅exp(bt) such that a = Db
 
-# change to stochastic where ⅊ is noise:
-# ∂Ψ/∂t = D⋅∂²Ψ/∂x² + ⅊
-# can only be solved analytically, use Runge-Kutta 4 or Euler's method.
+    # change to stochastic where ⅊ is noise:
+    # ∂Ψ/∂t = D⋅∂²Ψ/∂x² + ⅊
+    # can only be solved analytically, use Runge-Kutta 4 or Euler's method.
 
-a = 1;
-b = 1;
+    a = 1;
+    b = 1;
+end
