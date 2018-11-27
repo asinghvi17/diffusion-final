2ba894c1a3e7422b8ed960f7761e8654ae715b3d
asinghvi17 <anshulsinghvi@gmail.com>
2018-11-27 10:19:07 -0500

    added license to the top of both files

--- src/h.jl
+++ src/h.jl
@@ -1,3 +1,18 @@
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#         Version 2, December 2004
+#
+# Copyright (C) 2018 Anshul Singhvi <asinghvi17@simons-rock.edu>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+
 # This file essentially wraps all the imports in a separate file.
 
 # load packages
@@ -10,7 +25,7 @@ using   Plots,                    # for plotting
 
 
 # define Cell struct and methods related to it.
-mutable struct cell{T} <: Real
+mutable struct Block {T}
 
     Q :: T   # heat
 
