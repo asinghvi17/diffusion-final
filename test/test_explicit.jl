8166c634296de199068e8e9ffa20a82f4386c7fe
asinghvi17 <anshulsinghvi@gmail.com>
2018-12-01 15:02:49 -0500

    Commented out code

--- test/test_explicit.jl
+++ test/test_explicit.jl
@@ -1,8 +1,8 @@
-include.(["../src/external.jl"])
+include.(["../src/explicit.jl"])
 
 a = [Block(1, 1), Block(0, 1), Block(0, 1), Block(0, 1), Block(0, 1), Block(0, 1)]
-
+b = deepcopy(a)
 for i in 1:10
-    print(string(map(x -> string(x.Q/x.D)*" ", a)))
+    print(Base.string(map(x -> Base.string(x.Q/x.D)*" ", a)))
     a = step_1s_p(a)
 end
