68b24e438f3a4ad4384dd8ee57479579c3662c35
asinghvi17 <anshulsinghvi@gmail.com>
2018-12-01 15:19:30 -0500

    Removed commented out, old method

--- src/implicit.jl
+++ src/implicit.jl
@@ -2,28 +2,6 @@
 
 include.(["h.jl"])
 
-# """
-#     step_1s_p(arr::Array{Block, 1})
-#
-# This function will step a periodic array of Blocks in one spatial dimension by 1 time-increment.
-# It assumes that arr is periodic, i.e., that the neighbours of arr[end] are arr[end-1] and arr[1].
-# It will iterate from 1 to the end of the array.
-# """
-# function step_1s_p(
-#     arr::Array{Block, 1}
-#     )
-#
-#     k = 1
-#     h = 1
-#     r = k/h^2
-#     ret = deepcopy(arr); # copy the contents of arr into ret, to avoid len(arr) constructor calls
-#
-#     for i ∈ (length(A)-1):-1:1
-#
-#     end
-#
-# end
-
 # We will deal with matrices, since they are better to use and express this kind of thing more simply.
 # We begin by defining the matrix A, which will serve as the weight matrix for our system.
 
